8bb6d1d707bcfaaf44f3352d05eff067387da6da
Diego Arias <diegoalexd04@gmail.com>
2025-09-19 21:25:14 -0300

    guardarUsuariosDeLaBaseDeDatos

--- src/Modelo/AfinidadMusical.java
+++ src/Modelo/AfinidadMusical.java
@@ -1,6 +1,8 @@
 package Modelo;
 
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -10,19 +12,21 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class AfinidadMusical {
-	   public static void main(String[] args) {
-	       try (Reader reader = new FileReader("C:\\Users\\diego\\OneDrive\\Documentos\\GitHub\\Tp2---Musical-Affinity\\src\\Main\\BaseDeDatosDeUsuarios")) {
-	            Gson gson = new Gson();
-
-	            Type tipoLista = new TypeToken<List<Usuario>>(){}.getType();
-	            List<Usuario> usuarios = gson.fromJson(reader, tipoLista);
-	            ArrayList<Usuario> usuario2 = (ArrayList<Usuario>) usuarios;
-	            System.out.println(usuario2.get(0).interes_genero_urbano);
-	            
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
+	List<Usuario> usuarios;
+	
+	public void guardarUsuariosDeLaBaseDeDatos() {
+		InputStream input = AfinidadMusical.class.getResourceAsStream("BaseDeDatosDeUsuarios.json");
+		if (input == null) {
+			System.out.println("No se encontró el archivo");
+			return;
+		}
+       
+		InputStreamReader reader = new InputStreamReader(input);
+		Gson gson = new Gson();
+		
+		Type tipoLista = new TypeToken<List<Usuario>>(){}.getType();
+		this.usuarios = gson.fromJson(reader, tipoLista);
+    }
 	
 
 }
