d99e86e4a0f6ba3d04116dd3e8a5b841edd94ba0
Gabriel Nicolas Acevedo <88168569+RedNickTechnologies@users.noreply.github.com>
2025-09-21 09:49:03 -0300

    añadiendo clase usuario (commit prueba)

---
+++ Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+
+    private String nombre;
+
+    private int tango, folclore, rock, urbano;
+
+    public Usuario(String nombre, int t, int f, int r, int u) {
+        this.nombre = nombre;
+        this.tango = t;
+        this.folclore = f;
+        this.rock = r;
+        this.urbano = u;
+
+    }
+}
