ae80f64bacd2df4e0999fa724b3bbb931d8014f3
Diego Arias <diegoalexd04@gmail.com>
2025-10-01 18:55:32 -0300

    UI del tuto

---
+++ src/Visual/UISwing.java
@@ -0,0 +1,174 @@
+package Visual;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.FlatCarbonIJTheme;
+import com.formdev.flatlaf.intellijthemes.FlatMaterialDesignDarkIJTheme;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialLighterIJTheme;
+
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JSeparator;
+import java.awt.Cursor;
+import java.awt.Font;
+
+public class UISwing extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel background;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+		    UIManager.setLookAndFeel( new FlatCarbonIJTheme() );
+		} catch( Exception ex ) {
+		    System.err.println( "Failed to initialize LaF" );
+		}
+		UIManager.put("Button.arc", 0);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UISwing frame = new UISwing();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UISwing() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1050, 700);
+		background = new JPanel();
+		background.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(background);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(06, 57, 64));
+		
+		JSeparator separator = new JSeparator();
+		
+		JButton UsuariosBoton = new JButton("Usuarios");
+		UsuariosBoton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		UsuariosBoton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		UsuariosBoton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		UsuariosBoton.setBorderPainted(false);
+		UsuariosBoton.setBorder(null);
+		
+		JButton GrafoButton = new JButton("Grafo");
+		GrafoButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		GrafoButton.setBorderPainted(false);
+		GrafoButton.setBorder(null);
+		GrafoButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GrafoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(25, 94, 99));
+		
+		JButton RegistroUsuariosButton = new JButton("Registrar Usuario");
+		RegistroUsuariosButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		RegistroUsuariosButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		RegistroUsuariosButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		RegistroUsuariosButton.setBorderPainted(false);
+		RegistroUsuariosButton.setBorder(null);
+		ImageIcon originalIcon = new ImageIcon(getClass().getResource("/img/logo.png"));
+
+		Image scaledImage = originalIcon.getImage().getScaledInstance(220, 75, Image.SCALE_SMOOTH);
+		ImageIcon scaledIcon = new ImageIcon(scaledImage);
+		JLabel LogoLabel = new JLabel();
+		LogoLabel.setIcon(scaledIcon);
+		getContentPane().add(LogoLabel);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(21)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 221, GroupLayout.PREFERRED_SIZE))
+				.addComponent(GrafoButton, GroupLayout.PREFERRED_SIZE, 263, GroupLayout.PREFERRED_SIZE)
+				.addComponent(UsuariosBoton, GroupLayout.PREFERRED_SIZE, 263, GroupLayout.PREFERRED_SIZE)
+				.addComponent(RegistroUsuariosButton, GroupLayout.PREFERRED_SIZE, 263, GroupLayout.PREFERRED_SIZE)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(LogoLabel, GroupLayout.PREFERRED_SIZE, 240, GroupLayout.PREFERRED_SIZE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(21)
+					.addComponent(LogoLabel)
+					.addGap(18)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
+					.addGap(51)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(51)
+							.addComponent(GrafoButton, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE))
+						.addComponent(UsuariosBoton, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(100)
+							.addComponent(RegistroUsuariosButton, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE))))
+		);
+		panel.setLayout(gl_panel);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(236, 225, 195));
+		GroupLayout gl_background = new GroupLayout(background);
+		gl_background.setHorizontalGroup(
+			gl_background.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_background.createSequentialGroup()
+					.addGroup(gl_background.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_background.createSequentialGroup()
+							.addGap(5)
+							.addComponent(LogoLabel, GroupLayout.PREFERRED_SIZE, 240, GroupLayout.PREFERRED_SIZE))
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_background.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 766, Short.MAX_VALUE)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 766, GroupLayout.PREFERRED_SIZE)))
+		);
+		gl_background.setVerticalGroup(
+			gl_background.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_background.createSequentialGroup()
+					.addGap(16)
+					.addComponent(LogoLabel))
+				.addComponent(panel, GroupLayout.PREFERRED_SIZE, 672, GroupLayout.PREFERRED_SIZE)
+				.addGroup(gl_background.createSequentialGroup()
+					.addGap(43)
+					.addGroup(gl_background.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_background.createSequentialGroup()
+							.addGap(124)
+							.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 505, Short.MAX_VALUE))
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)))
+		);
+		background.setLayout(gl_background);
+		
+	}
+}
